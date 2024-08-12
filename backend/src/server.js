1d109a14ef410924000c424f3f03a234ef1d8484
HuyVo1231 <vonhathuy1306@gmail.com>
2024-08-12 10:20:23 +0700

    Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
+import path from 'path'
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import route from './routes/index.js'
 import cors from 'cors'
+const __dirname = path.resolve()
+
 // database
 import { connect } from './config/db/index.js'
 import { app, server } from './socket/socket.js'
@@ -21,6 +24,10 @@ const PORT = process.env.PORT || 5000
 
 // routes
 route(app)
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+})
 // connect to database
 connect()
 
